b8644a2be9b2bb501e9c4c9873a100a4e6e011d3
FAVOUR <truelyfavoured@gmail.com>
2020-08-23 17:38:28 +0100

    Done with the DataTransferObject.kt class

---
+++ app/src/main/java/com/example/androiddevbyte/network/DataTransferObject.kt
@@ -0,0 +1,46 @@
+package com.example.androiddevbyte.network
+
+import androidx.lifecycle.Transformations.map
+import com.example.androiddevbyte.database.DatabaseVideo
+import com.example.androiddevbyte.domain.DevByteVideo
+import com.squareup.moshi.JsonClass
+
+
+ @JsonClass(generateAdapter = true)
+ data class NetworkVideoContainer (val videos: List<NetworkVideo>)
+
+/**
+ * Videos represent a devbyte that can be played.
+ */
+@JsonClass(generateAdapter = true)
+data class NetworkVideo(
+    val title: String,
+    val description: String,
+    val url: String,
+    val updated: String,
+    val thumbnail: String,
+    val closedCaptions: String?)
+
+
+ fun NetworkVideoContainer.addDomainModel():List<DevByteVideo>{
+
+     return videos.map {
+            DevByteVideo(
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                updated = it.updated,
+                thumbnail = it.thumbnail)
+     }
+ }
+
+ fun NetworkVideoContainer.asDataBaseModel():List<DatabaseVideo>{
+     return videos.map {
+         DatabaseVideo(
+             title = it.title,
+             description = it.description,
+             url = it.url,
+             updated = it.updated,
+             thumbnail = it.thumbnail)
+     }
+ }

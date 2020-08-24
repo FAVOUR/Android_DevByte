832f4306053775458f40890a002247fea7c82591
FAVOUR <truelyfavoured@gmail.com>
2020-08-24 23:24:48 +0100

    Created a new Package Called UI and Renamed the main Activity to DevByteActivity.kt

--- app/src/main/java/com/example/androiddevbyte/ui/DevByteActivity.kt
+++ app/src/main/java/com/example/androiddevbyte/ui/DevByteActivity.kt
@@ -1,7 +1,8 @@
-package com.example.androiddevbyte
+package com.example.androiddevbyte.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androiddevbyte.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

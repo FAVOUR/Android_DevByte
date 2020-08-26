e6f8efe1f8141bf74a9fa8ad742ba9fe4a72432e
FAVOUR <truelyfavoured@gmail.com>
2020-08-26 02:58:22 +0100

    Done with setting uo the viewModel

--- app/src/main/java/com/example/androiddevbyte/ui/DevByteViewModel.kt
+++ app/src/main/java/com/example/androiddevbyte/ui/DevByteViewModel.kt
@@ -1,7 +1,146 @@
 package com.example.androiddevbyte.ui
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.androiddevbyte.domain.DevByteVideo
+import com.example.androiddevbyte.network.DevByteNetwork
+import com.example.androiddevbyte.network.addDomainModel
+import com.example.androiddevbyte.network.asDataBaseModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.lang.IllegalArgumentException
 
-class DevByteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+/**
+ * DevByteViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
+ * allows data to survive configuration changes such as screen rotations. In addition, background
+ * work such as fetching network results can continue through configuration changes and deliver
+ * results after the new Fragment or Activity is available.
+ *
+ * @param application The application that this viewmodel is attached to, it's safe to hold a
+ * reference to applications across rotation since Application is never recreated during actiivty
+ * or fragment lifecycle events.
+ */
+class DevByteViewModel(application: Application) :AndroidViewModel(application) {
+
+    /**
+     * This is the job for all coroutines started by this ViewModel.
+     *
+     * Cancelling this job will cancel all coroutines started by this ViewModel.
+     */
+    private val viewModelJob = SupervisorJob()
+
+    /**
+     * This is the main scope for all coroutines lunched by MainViewmodel .
+     *
+     * Since we pass viewmodelJob, you can cancel  all couroutines lunched by uiScope by calling
+     *  viewmodelJob.cancel()
+     */
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    /**
+     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private val _playList = MutableLiveData<List<DevByteVideo>>()
+
+    /**
+     * A playlist of videos that can be shown on the screen. Views should use this to get access
+     * to the data.
+     */
+    val playList: LiveData<List<DevByteVideo>>
+        get() = _playList
+
+
+    /**
+     * Event triggered for network error. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+
+    /**
+     * Event triggered for network error,Views should use this to get Access to data
+     */
+
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+
+    /**
+     * Flag to display the error message. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    /**
+     * Flag to display the error message. Views should use this to get access
+     * to the data.
+     */
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+
+    /**
+     * init{} is called immediately when this ViewModel is created.
+     */
+    init {
+        refreshDataFromNetwork()
+    }
+
+
+    /**
+     * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
+     * background thread.
+     */
+
+    private fun refreshDataFromNetwork() = viewModelScope.launch {
+        try {
+            val playlist = DevByteNetwork.devbytes.getPlaylist().await()
+
+            _playList.value = playlist.addDomainModel()
+//            _playList.postValue(playlist.addDomainModel())
+
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+        } catch (e: IOException) {
+            // Show a Toast error message and hide the progress bar.
+            _eventNetworkError.value = true
+        }
+    }
+
+    /**
+     * Resets the network error flag.
+     */
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+
+    /**
+     * Cancel all coroutines when the ViewModel is cleared
+     */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+}
+    /**
+     * Factory for constructing DevByteViewModel with parameter
+     */
+
+    class Factory(val  app:Application):ViewModelProvider.Factory{
+
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(DevByteViewModel::class.java)){
+                @Suppress("UNCHECKED_CAST")
+                return  DevByteViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct ViewModel")
+    }
+
 }

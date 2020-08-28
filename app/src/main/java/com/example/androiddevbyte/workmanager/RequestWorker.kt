5a94f1eeca804f6ca0b7124be1c65c061d13d216
FAVOUR <truelyfavoured@gmail.com>
2020-08-28 23:34:32 +0100

    Done with the Worker Class

--- app/src/main/java/com/example/androiddevbyte/workmanager/RequestWorker.kt
+++ app/src/main/java/com/example/androiddevbyte/workmanager/RequestWorker.kt
@@ -1,13 +1,28 @@
 package com.example.androiddevbyte.workmanager
 
+import android.app.Application
 import android.content.Context
 import androidx.work.CoroutineWorker
-import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.androiddevbyte.database.getDatabase
+import com.example.androiddevbyte.repository.VideoRepository
+import com.example.androiddevbyte.ui.DevByteViewModel
+import retrofit2.HttpException
+import java.net.HttpCookie
 
-class RequestWorker(context: Context,params:WorkerParameters):CoroutineWorker(context,params) {
+class RequestWorker(val context: Context,params:WorkerParameters):CoroutineWorker(context,params) {
 
     override suspend fun doWork(): Result {
+
+        val database = getDatabase(context = applicationContext)
+        val repository =VideoRepository(database = database)
+
+         try {
+             repository.refreshVideos()
+         }  catch (e :HttpException) {
+             Result.retry()
+         }
+
 
 
         return  Result.success()
